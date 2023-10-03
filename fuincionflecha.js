6eba1f60e57b9a3cf92c6729009bd0ba3ca67fa2
johannacuartasdeveloper <140031481+JOHANNACUARTASDEV@users.noreply.github.com>
2023-10-03 07:12:13 -0500

    Create fuincionflecha.js

---
+++ fuincionflecha.js
@@ -0,0 +1,14 @@
+const notas=["2.8","5","4.5","3.0","4.9"];
+
+//para recorrer el array utilizo el foreach
+notas.forEach(function(notas){ 
+    console.log(notas)
+});
+
+//utilizando la flecha =>
+notas.forEach((notas) => {
+    console.log(notas)});
+
+    //para reducir el codigo utilizo 
+    notas.forEach(notas) => {
+        console.log(notas)};
